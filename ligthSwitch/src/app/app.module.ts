cadfe0c08a44bf79be62e1006a4b8063033c5d6c
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-10 09:33:45 +0100

    added structure

---
+++ ligthSwitch/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TrafficLightComponent } from './components/traffic-light/traffic-light.component';
+import { ControllerComponent } from './components/controller/controller.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TrafficLightComponent,
+    ControllerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
