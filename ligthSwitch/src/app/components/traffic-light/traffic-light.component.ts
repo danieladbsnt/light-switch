dd74e3220f5816ec4ea6b61f4f9a906363588dc2
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-11 13:29:18 +0100

    injected service

--- ligthSwitch/src/app/components/traffic-light/traffic-light.component.ts
+++ ligthSwitch/src/app/components/traffic-light/traffic-light.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from 'src/app/service.service';
 
 @Component({
   selector: 'app-traffic-light',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./traffic-light.component.css']
 })
 export class TrafficLightComponent implements OnInit {
-
-  constructor() { }
+colors: string[] = [];
+  constructor(private service: ServiceService) { 
+    service.sharingDataObservable.subscribe(
+      data => this.colors = data
+    )
+  }
 
   ngOnInit(): void {
   }
+
+/*
+cuando el value del option del controller.component es de X color, tengo que 
+añadir la clase de ese color al div de ese color. Es decir, cuando el option
+está seleccionado en el value red, tengo que añadir la clase red al div-red.
+*/
 
 }
