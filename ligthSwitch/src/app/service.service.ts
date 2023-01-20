480ab3eab10426bdc00f4e9535eb53b3b21319b4
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-20 13:56:06 +0100

    cleaned code

--- ligthSwitch/src/app/service.service.ts
+++ ligthSwitch/src/app/service.service.ts
@@ -7,7 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 export class ServiceService {
   constructor() {}
 
-time: number = 0;
+timer: any;
 interval: any;
 
   private sharingDataPrivate: BehaviorSubject<string[]> = new BehaviorSubject
@@ -20,14 +20,4 @@ interval: any;
   set sharingData(data: any) {
     this.sharingDataPrivate.next(data);
   }
-
-// startTimer() {
-//   this.interval = setInterval(() => {
-//     console.log('hola');
-//   }, 1000)
-// }
-
-// pauseTimer() {
-//   clearInterval(this.interval)
-// }
 }
