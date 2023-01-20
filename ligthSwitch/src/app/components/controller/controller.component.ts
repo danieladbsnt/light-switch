944f219f6419fcc3a911822f6378f02e0614ad1d
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-20 13:56:31 +0100

    added unsuscribe method and cleaned code

--- ligthSwitch/src/app/components/controller/controller.component.ts
+++ ligthSwitch/src/app/components/controller/controller.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { ServiceService } from 'src/app/service.service';
 
 @Component({
@@ -6,18 +7,18 @@ import { ServiceService } from 'src/app/service.service';
   templateUrl: './controller.component.html',
   styleUrls: ['./controller.component.css']
 })
-export class ControllerComponent {
+export class ControllerComponent implements OnInit, OnDestroy  {
 value!: boolean;
 colors: string[] = [];
 selected: string = '';
 timer: any;
 i = 0;
 
+private _unsuscribe$ = new Subject<boolean>();
+
   constructor(private service: ServiceService) {}
 
    ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.service.sharingDataObservable.subscribe(
       data => this.colors = data
     )
@@ -28,8 +29,8 @@ onSelect(ev:any) {
     this.value = true;
     this.timer = setInterval(() => {
        if (this.value = true && this.colors[this.i] != undefined) {
-        this.selected = this.colors[this.i]
-        console.log(this.i++)
+        this.selected = this.colors[this.i];
+        this.i++;
        } 
     }, 1000
     )
@@ -39,9 +40,8 @@ onSelect(ev:any) {
   }
 }
 
-ngOnDestroy(): void {
-  //Called once, before the instance is destroyed.
-  //Add 'implements OnDestroy' to the class.
-  
-}
+  ngOnDestroy(): void {
+    this._unsuscribe$.next(true)
+    this._unsuscribe$.complete();
+  }
 }
