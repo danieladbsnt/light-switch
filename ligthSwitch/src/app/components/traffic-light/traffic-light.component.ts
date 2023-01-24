5ad3bba4a92a6fa455e05e76a784272adb0830b4
danieladbsnt <daniela.darnea@bosonit.com>
2023-01-24 11:35:49 +0100

    cleaned code

--- ligthSwitch/src/app/components/traffic-light/traffic-light.component.ts
+++ ligthSwitch/src/app/components/traffic-light/traffic-light.component.ts
@@ -15,8 +15,6 @@ color1: string = 'gray';
 color2: string = 'gray';
 color3: string = 'gray';
 
-// sendData = new BehaviorSubject(this.test());
-
 private _unsuscribe$ = new Subject<boolean>();
 
   constructor(private service: ServiceService) {}
@@ -27,9 +25,6 @@ private _unsuscribe$ = new Subject<boolean>();
     )
   }
 
-// communicateData() {
-//   this.sendData.next()
-// }
 //pasar esta función al hermano1 par que se haga cuando hermano1 le de click al checkbox¿?
 test() {
     this.timer = setInterval(() => {
@@ -52,11 +47,6 @@ test() {
     )
 }
 
-/*
-cuando el value del option del controller.component es de X color, tengo que 
-añadir la clase de ese color al div de ese color. Es decir, cuando el option
-está seleccionado en el value red, tengo que añadir la clase red al div-red.
-*/
 ngOnDestroy(): void {
   this._unsuscribe$.next(true)
   this._unsuscribe$.complete();
