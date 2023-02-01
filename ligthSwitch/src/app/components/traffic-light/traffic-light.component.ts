cecd6f6f70d7fef190d4261be2cda7f147f48438
danieladbsnt <daniela.darnea@bosonit.com>
2023-02-01 12:52:47 +0100

    changed main functionality to service

--- ligthSwitch/src/app/components/traffic-light/traffic-light.component.ts
+++ ligthSwitch/src/app/components/traffic-light/traffic-light.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { ServiceService } from 'src/app/service.service';
 
 @Component({
@@ -8,44 +8,19 @@ import { ServiceService } from 'src/app/service.service';
   styleUrls: ['./traffic-light.component.css']
 })
 export class TrafficLightComponent implements OnInit, OnDestroy {
-colors: string[] = [];
-timer: any;
-i = 0;
-color1: string = 'gray';
-color2: string = 'gray';
-color3: string = 'gray';
-
+selectedColor!: string;
 private _unsuscribe$ = new Subject<boolean>();
 
   constructor(private service: ServiceService) {}
 
   ngOnInit(): void {
-    this.service.sharingDataObservable.subscribe(
-      data => this.colors = data
-    )
+    this.service.sharingData$.pipe(takeUntil(this._unsuscribe$))
+      .subscribe({ next: (color) => this.selectedColor = color})    
   }
 
-//pasar esta función al hermano1 par que se haga cuando hermano1 le de click al checkbox¿?
-test() {
-    this.timer = setInterval(() => {
-       if (this.colors[this.i] != undefined && this.colors[this.i] == 'Red') {
-        console.log('red');
-        this.color1 = 'red'
-        this.i++;
-       } else if (this.colors[this.i] != undefined && this.colors[this.i] == 'Orange') {
-        this.color1 = 'gray'
-        this.color2 = 'orange'
-        this.i++;
-        console.log('orange');
-       } else if (this.colors[this.i] != undefined && this.colors[this.i] == 'Green') {
-        this.color2 = 'gray'
-        this.color3 = 'green'
-        this.i++;
-        console.log('green');
-       }
-    }, 1000
-    )
-}
+  changeColor(ev:any) {
+    this.service.onSelect(ev)
+  }
 
 ngOnDestroy(): void {
   this._unsuscribe$.next(true)
