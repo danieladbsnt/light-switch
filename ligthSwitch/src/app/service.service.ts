7f9e7b10e1f7436bd2d88ffb594c0f52e67b6c39
danieladbsnt <daniela.darnea@bosonit.com>
2023-02-02 10:14:11 +0100

    cleaned code

--- ligthSwitch/src/app/service.service.ts
+++ ligthSwitch/src/app/service.service.ts
@@ -8,13 +8,11 @@ export class ServiceService {
 colors = ['red', 'orange', 'green']
 timer: any;
 i = 0;
-
 sharingData$ = new Subject<string>();
 
 constructor() {}
-//hacer una sola funcion que incluya ambos casos y suscribirme luego en ambos...
+
 onSelect(on:boolean) {
-  console.log('aa');
   if (on) {
     this.timer = setInterval(() => {
           this.sharingData$.next(this.colors[this.i]);
@@ -28,6 +26,5 @@ onSelect(on:boolean) {
     clearInterval(this.timer)
   }
 }
-
 
 }
